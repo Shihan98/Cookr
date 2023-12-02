d9a6ac74a0c3e6e5c10969647ecaf655d93b6b2a
Shihan98 <shihanthathsara98@gmail.com>
2023-12-02 20:26:18 +0530

    feat: add getall recipe endpoint and functions

--- Cookr-backend/src/main/java/com/recipe/Cookrbackend/service/impl/RecipeServiceImpl.java
+++ Cookr-backend/src/main/java/com/recipe/Cookrbackend/service/impl/RecipeServiceImpl.java
@@ -6,28 +6,40 @@ import com.recipe.Cookrbackend.repository.RecipeRepo;
 import com.recipe.Cookrbackend.service.RecipeService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.TypeToken;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class RecipeServiceImpl implements RecipeService {
-    @Autowired
+
     private RecipeRepo recipeRepo;
 
-    @Autowired
     private ModelMapper modelMapper;
 
     @Override
     public String saveRecipe(RecipeDto recipeDto) {
         Recipe recipe = modelMapper.map(recipeDto,Recipe.class);
-        recipeRepo.save(recipe);
-        return "succefully saved";
-//        if(recipe.getId() == null || !recipeRepo.existsById(recipe.getId())){
-//            recipeRepo.save(recipe);
-//            return "succefully saved";
-//        }else {
-//            throw new DuplicateKeyException("Already Saved");
-//        }
+        if(recipe.getId() == null || !recipeRepo.existsById(recipe.getId())){
+            recipeRepo.save(recipe);
+            return "succefully saved";
+        }else {
+            throw new DuplicateKeyException("Already Saved");
+        }
+    }
+
+    @Override
+    public List<RecipeDto> getAllRecipe() {
+        List<Recipe> allRecipe = recipeRepo.findAll();
+        if(!allRecipe.isEmpty()){
+            List<RecipeDto> allRecipeDto = modelMapper.map(allRecipe,new TypeToken<List<RecipeDto>>(){}.getType());
+            return  allRecipeDto;
+        }
+        else {
+            throw new RuntimeException("Not Found");
+        }
     }
 }
